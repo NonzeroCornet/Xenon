c702eb962cc4801e863d9a143f874b748fc5999a
NonzeroCornet34 <ullerbri000@gmail.com>
2022-08-01 22:42:19 -0600

    Added xenoncard.js library

---
+++ public/xenoncard.js
@@ -0,0 +1,12 @@
+function cardData(data) {
+    return decodeURIComponent(
+        window.location.href
+            .split("?")[0]
+            .split(data + ":")[0]
+            .split("%3B")[0]
+    );
+}
+
+function nextCard() {
+    window.parent.postMessage("nextCard", "*");
+}
